57420ae6fedae9cf7c878af9601ef2e779c39833
andyrodrigues30 <andrea.melissa.rodrigues@gmail.com>
2026-05-01 14:10:22 +0100

    Ref #22 - rename to settings device row

--- src/ui/SettingsDeviceRow.ts
+++ src/ui/SettingsDeviceRow.ts
@@ -2,7 +2,7 @@ import { Setting } from "obsidian";
 import FloppyDiskPlugin from "../main";
 import { Device } from "types/device";
 
-export class DeviceRow {
+export class SettingsDeviceRow {
   private plugin: FloppyDiskPlugin;
   private containerEl: HTMLElement;
   private deviceId: string;
@@ -34,7 +34,7 @@ export class DeviceRow {
           .setWarning()
           .setButtonText("Revoke")
           .onClick(async () => {
-            this.plugin.deviceManager.removeDevice(device.id);
+            await this.plugin.deviceManager.removeDevice(device.id);
           })
       );
     }
@@ -45,7 +45,7 @@ export class DeviceRow {
         .setIcon("trash")
         .setTooltip("Delete device")
         .onClick(async (): Promise<void> => {
-          this.plugin.deviceManager.removeDevice(device.id);
+          await this.plugin.deviceManager.removeDevice(device.id);
         })
     );
   }
